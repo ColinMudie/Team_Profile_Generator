e0de79a218668c8d0663fdd98ef2efd4310bcb66
Colin Mudie <csmudie1@gmail.com>
2021-02-11 12:30:48 -0500

    created skeleton for classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const inquirer = require('inquirer')
+
+//Classes
+const Employee = require('./lib/employee');
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+
