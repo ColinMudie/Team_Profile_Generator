2d88556e70f9a832dc749b6be72e41297364aefd
Colin Mudie <csmudie1@gmail.com>
2021-02-14 19:10:06 -0500

    added style for dynamic cards

--- scripts/generate-html.js
+++ scripts/generate-html.js
@@ -10,12 +10,12 @@ function generateManagers(managers) {
     if (managers.length > 0) {
         managers.forEach(element => {
             managerCard = managerCard.concat(`
-            <div class="card" style="width: 18rem;">
+            <div class="card card-custom" style="width: 18rem;">
             <div class="card-body">
-                <h3 class="card-title">${element.getName()}</h3>
-                <p class="card-text">Role: ${element.getRole()}</p>
+                <h2 class="card-title">${element.getName()}</h2>
+                <h4 class="card-text">${element.getRole()}</h4>
                 <p class="card-text">ID: ${element.getId()}</p>
-                <p class="card-text">Email: ${element.getEmail()}</p>
+                <p class="card-text">Email: <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
                 <p class="card-text">Office Number:  ${element.getOfficeNumber()}</p>
             </div>
             </div>
@@ -30,13 +30,13 @@ function generateEngineers(engineers) {
     if (engineers.length > 0) {
         engineers.forEach(element => {
             engineerCard = engineerCard.concat(`
-            <div class="card" style="width: 18rem;">
+            <div class="card card-custom" style="width: 18rem;">
             <div class="card-body">
-                <h3 class="card-title">${element.getName()}</h3>
-                <p class="card-text">Role: ${element.getRole()}</p>
+                <h2 class="card-title">${element.getName()}</h2>
+                <h4 class="card-text">${element.getRole()}</h4>
                 <p class="card-text">ID: ${element.getId()}</p>
-                <p class="card-text">Email: ${element.getEmail()}</p>
-                <p class="card-text">Github: ${element.getGithub()}</p>
+                <p class="card-text">Email: <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
+                <p class="card-text">Github: <a href="${element.getGithub()}" target="_blank">${element.getGithub()}</a></p>
             </div>
             </div>
             `)
@@ -50,12 +50,12 @@ function generateInterns(interns) {
     if (interns.length > 0) {
         interns.forEach(element => {
             internsCard = internsCard.concat(`
-            <div class="card" style="width: 18rem;">
+            <div class="card card-custom" style="width: 18rem;">
             <div class="card-body">
-                <h3 class="card-title">${element.getName()}</h3>
-                <p class="card-text">Role: ${element.getRole()}</p>
+                <h2 class="card-title">${element.getName()}</h2>
+                <h4 class="card-text">${element.getRole()}</h4>
                 <p class="card-text">ID: ${element.getId()}</p>
-                <p class="card-text">Email: ${element.getEmail()}</p>
+                <p class="card-text">Email: <a href="mailto:${element.getEmail()}">${element.getEmail()}</a></p>
                 <p class="card-text">School: ${element.getSchool()}</p>
             </div>
             </div>
@@ -77,9 +77,18 @@ function generateHtml(managers, engineers, interns) {
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     </head>
     <body>
-        ${generateManagers(managers)}
-        ${generateEngineers(engineers)}
-        ${generateInterns(interns)}
+        <div class="jumbotron jumbotron-fluid jumbotron-custom">
+            <div class="container">
+                <h1 class="display-4">My Team</h1>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                ${generateManagers(managers)}
+                ${generateEngineers(engineers)}
+                ${generateInterns(interns)}
+            </div>
+        </div>
     </body>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
